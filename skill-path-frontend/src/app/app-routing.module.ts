e7a8b8d06d59e19ddf5238a172a9c7c6b73ac521
bmuntean <beniamin.munteanu@keysight.com>
2021-03-27 09:39:25 +0200

    .

--- skill-path-frontend/src/app/app-routing.module.ts
+++ skill-path-frontend/src/app/app-routing.module.ts
@@ -26,9 +26,10 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
-      preloadingStrategy: PreloadAllModules,
-    }),
+    useHash: true,
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+}),
   ],
   exports: [RouterModule],
 })
