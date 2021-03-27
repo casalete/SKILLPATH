a58bfc611f0769eefd1d1aae14a5bd36e038ea7b
bmuntean <beniamin.munteanu@keysight.com>
2021-03-27 20:02:20 +0200

    .

---
+++ skill-path-backend/src/auth/authService.ts
@@ -0,0 +1,25 @@
+import passport from 'passport';
+
+exports.auth = () => {
+    return (req, res, next) => {
+        passport.authenticate('local', (error, user, info) => {
+            if (error) res.status(400).json({ statusCode: 200, message: error });
+            req.login(user, function (error) {
+                if (error) return next(error);
+                next();
+            });
+        })(req, res, next);
+    };
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return res.status(401).json({ statusCode: 400, message: 'not authenticated' });
+};
+
+export interface user {
+    test: string;
+    asd: boolean;
+}
