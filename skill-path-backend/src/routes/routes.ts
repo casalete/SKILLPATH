3c59707ca76be50cda9ec2e2123f6fbb373d3673
bogdan <bogdan.sandu@cognyte.com>
2021-03-30 00:55:16 +0300

    add user authentication and login

--- skill-path-backend/src/routes/routes.ts
+++ skill-path-backend/src/routes/routes.ts
@@ -1,177 +1,59 @@
-import express from 'express'
+import express, { application } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import {usersRouter} from "./User/user-routes";
+import {secureRouter} from "./secure-routes";
+
 
 export const router = express.Router();
+const app = express();
 
 
-const elastic = require('@elastic/elasticsearch');
+app.use('/users', usersRouter);
+app.use('/profile', secureRouter);
 
-const bodyParser = require('body-parser').json();
 
-const elasticClient = new elastic.Client({
-    node: 'http://localhost:9200',
-});
+router.post(
+    '/signup',
+    passport.authenticate('signup', { session: false }),
+    async (req, res, next) => {
+      res.json({
+        message: 'Signup successful',
+        user: req.user
+      });
+    }
+  );
 
-/* app.use('/users', UsersRouter)
- */
-let products = [
-    {
-        id: '1',
-        name: 'Chelutu',
-        categories: ['caine', 'mamifer'],
-        description: 'Cainele lui Dorian',
-    },
-    {
-        id: '2',
-        name: 'Dorian',
-        categories: ['om', 'mamifer'],
-        description: 'Stapanul lui Chelutu',
-    },
-    {
-        id: '3',
-        name: 'Cori',
-        categories: ['vaca', 'mamifer'],
-        description: 'Ajutorul lui Dorian',
-    },
-];
+router.post(
+  '/login',
+  async (req, res, next) => {
+    passport.authenticate(
+      'login',
+      async (err, user, info) => {
+        try {
+          if (err || !user) {
+            const error = new Error('An error occurred.');
 
-router.use((req, res, next) => {
-    elasticClient
-        .index({
-            index: 'logs',
+            return next(error);
+          }
 
-            body: {
-                url: req.url,
-                method: req.method,
-            },
-        })
-        .then((res) => {
-            console.log('Logs indexed');
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+          req.login(
+            user,
+            { session: false },
+            async (error) => {
+              if (error) return next(error);
 
-    next();
-});
+              const body = { _id: user._id, email: user.email };
+              const token = jwt.sign({ user: body }, 'TOP_SECRET');
 
-router.post('/products', bodyParser, (req, res) => {
-    elasticClient
-        .index({
-            index: 'products',
-            body: req.body,
-        })
-        .then((resp) => {
-            return res.status(200).json({
-                msg: 'product indexed',
-            });
-        })
-        .catch((err) => {
-            return res.status(500).json({
-                msg: 'Error',
-                err,
-            });
-        });
-});
-
-router.get('/products/:id', (req, res) => {
-    let query = {
-        index: 'products',
-        id: req.params.id,
-    };
-    elasticClient
-        .get(query)
-        .then((resp) => {
-            if (!resp) {
-                return res.status(404).json({
-                    product: resp,
-                });
+              return res.json({ token });
             }
-            return res.status(200).json({
-                product: resp,
-            });
-        })
-        .catch((err) => {
-            return res.status(500).json({
-                msg: 'Error not found',
-                err,
-            });
-        });
-});
-
-router.put('/products/:id', bodyParser, (req, res) => {
-    elasticClient
-        .update({
-            index: 'products',
-            id: req.params.id,
-            body: {
-                doc: req.body,
-            },
-        })
-        .then((resp) => {
-            return res.status(200).json({
-                msg: 'product updated',
-            });
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json({
-                msg: 'Error',
-                err,
-            });
-        });
-});
-
-router.delete('/products/:id', (req, res) => {
-    elasticClient
-        .delete({
-            index: 'products',
-            id: req.params.id,
-        })
-        .then((resp) => {
-            res.status(200).json({
-                msg: 'Error',
-            });
-        })
-        .catch((err) => {
-            res.status(404).json({
-                msg: 'Error',
-            });
-        });
-});
-
-// router.get('/products', (req,res)=>{
-
-//     let query = {
-//         index: 'products'
-//     }
-
-//     if(req.query.product) query.q = `*${req.query.product}*`;
-//     elasticClient.search(query)
-//     .then(reps=>{
-//         return res.status(200).json({
-//             products: resp.hits.hits
-//         });
-//     })
-//     .catch(err=>{
-//         console.log(err);
-//         return res.status(500).json({
-//             msg: 'Error',
-//             err
-//         });
-//     });
-
-// });
-
-// module.exports = router;?
-
-
-/* export interface UserModel {
-    name: string,
-    id: number,
-}
-
-export interface PostModel {
-    name: string,
-    id: number,
-}
-export const user : UserModel ={name:'test', id:1}; */
+          );
+        } catch (error) {
+          return next(error);
+        }
+      }
+    )(req, res, next);
+  }
+);
