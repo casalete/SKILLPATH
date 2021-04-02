a452d242b8f27f3177dbe41920c0d20806ef3451
Beniamin Munteanu <beniamin.munteanu@keysight.com>
2021-04-02 09:05:51 +0300

    .

--- skill-path-frontend/src/app/app.component.ts
+++ skill-path-frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SubSink } from 'subsink';
+import { Store } from '@ngrx/store';
+import { selectRouteData } from './store';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  constructor(private store: Store) {}
+
+  navbar: boolean;
+  subs = new SubSink();
+
+  ngOnInit(): void {
+    this.subs.sink = this.store
+      .select(selectRouteData)
+      .pipe(filter((data) => data?.layout !== undefined))
+      .subscribe((data) => (this.navbar = data.layout.navbar));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
 }
--- skill-path-frontend/src/app/core/services/authService.ts
+++ skill-path-frontend/src/app/core/services/authService.ts
@@ -1,12 +1,24 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { UserCredentials } from '../Models/UserCredentials';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class AuthService {
   constructor(private http: HttpClient) {}
 
+  private apiUrl = `${environment.apiUrl}`;
+
   login(userCredentials: UserCredentials): Observable<any> {
-    // return this.http.post()
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.http.post(`/login`, userCredentials, httpOptions);
     return null;
   }
 }
