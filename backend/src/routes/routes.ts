5146e04809b9d90ebaddb9f022af577bf9dd32ad
Beniamin Munteanu <beniamin.munteanu@keysight.com>
2021-04-06 00:16:07 +0300

    .

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -5,9 +5,6 @@ import passport from 'passport';
 
 export const router = express.Router();
 
-// require('../auth/auth');
-
 router.use('/auth', authRouter);
 
 router.use('/users', passport.authenticate('jwt', { session: false }), usersRouter);
-router.use('/profile', secureRouter);
--- frontend/src/app/store/authentication/authentication.reducer.ts
+++ frontend/src/app/store/authentication/authentication.reducer.ts
@@ -4,59 +4,56 @@ import * as AuthActions from './authentication.actions';
 import { AuthState, initialState } from './authentication.state';
 
 const reducer = createReducer(
-  initialState,
-  on(AuthActions.loginStart, (state) => ({
-    ...state,
-    loading: true,
-    loadingStatus: 'Authenticating...',
-  })),
-  on(
-    AuthActions.authenticateSuccess,
-    (state, { authenticatedSessionData }) => ({
-      ...state,
-      authenticatedSessionData,
-      loading: false,
-      loadingStatus: null,
-    })
-  ),
-  on(AuthActions.authenticateError, (state, { error }) => ({
-    ...state,
-    authError: error,
-    authenticatedSessionData: null,
-    loading: false,
-    loadingStatus: null,
-    profile: null,
-    // TODO: check if this is still used
-  })),
-  on(AuthActions.logout, (state) => ({
-    ...state,
-    authenticatedSessionData: null,
-    loading: false,
-    loadingStatus: null,
-  })),
+    initialState,
+    on(AuthActions.loginStart, (state) => ({
+        ...state,
+        loading: true,
+        loadingStatus: 'Authenticating...',
+    })),
+    on(AuthActions.authenticateSuccess, (state, { authenticatedSessionData }) => ({
+        ...state,
+        authenticatedSessionData,
+        loading: false,
+        loadingStatus: null,
+    })),
+    on(AuthActions.authenticateError, (state, { error }) => ({
+        ...state,
+        authError: error,
+        authenticatedSessionData: null,
+        loading: false,
+        loadingStatus: null,
+        profile: null,
+        // TODO: check if this is still used
+    })),
+    on(AuthActions.logout, (state) => ({
+        ...state,
+        authenticatedSessionData: null,
+        loading: false,
+        loadingStatus: null,
+    })),
 
-  on(AuthActions.registerStart, (state) => ({
-    ...state,
-    loading: true,
-    loadingStatus: 'Registering...',
-  })),
+    on(AuthActions.registerStart, (state) => ({
+        ...state,
+        loading: true,
+        loadingStatus: 'Registering...',
+    })),
 
-  on(AuthActions.registerError, (state, { error }) => ({
-    ...state,
-    authError: error,
-    authenticatedSessionData: null,
-    loading: false,
-    loadingStatus: null,
-    // TODO: check if this is still used
-  })),
+    on(AuthActions.registerError, (state, { error }) => ({
+        ...state,
+        authError: error,
+        authenticatedSessionData: null,
+        loading: false,
+        loadingStatus: null,
+        // TODO: check if this is still used
+    })),
 
-  on(AuthActions.clearError, (state) => ({
-    ...state,
-    authError: null,
-    profileError: null,
-  }))
+    on(AuthActions.clearError, (state) => ({
+        ...state,
+        authError: null,
+        profileError: null,
+    }))
 );
 
 export function authReducer(state: AuthState, action: Action): any {
-  return reducer(state, action);
+    return reducer(state, action);
 }
