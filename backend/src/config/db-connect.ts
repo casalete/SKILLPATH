0a12779a9da7cac893a098f6f7b95834b7140f91
Beniamin Munteanu <beniamin.munteanu@keysight.com>
2021-04-06 23:56:38 +0300

    fixeroni

---
+++ backend/src/config/db-connect.ts
@@ -0,0 +1,14 @@
+import mongoose, { Connection } from 'mongoose';
+
+export default function connectToDatabase() {
+    mongoose
+        .connect('mongodb://localhost:27017/test', { useNewUrlParser: true })
+        .catch((err: any) => console.log(err.reason));
+
+    const db: Connection = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', () => {
+        console.log('Connected to MongoDB');
+    });
+}
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -7,4 +7,9 @@ export const router = express.Router();
 
 router.use('/auth', authRouter);
 
+// const test = (req, res, next) => {
+//     console.log(req);
+//     next();
+// };
+
 router.use('/users', passport.authenticate('jwt', { session: false }), usersRouter);
