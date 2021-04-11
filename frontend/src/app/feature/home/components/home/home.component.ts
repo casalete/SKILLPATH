be52ae2fea3d8230f08084c9c27ad0fc6ebd6443
Beniamin Munteanu <beniamin.munteanu@keysight.com>
2021-04-11 22:43:32 +0300

    frontened navbar and topics table

--- frontend/src/app/feature/home/components/home/home.component.ts
+++ frontend/src/app/feature/home/components/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= 8; i++) {
             this.elements.push({
                 id: i.toString(),
                 first: 'Wpis ' + i,
